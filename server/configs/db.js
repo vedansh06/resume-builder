bc68707e1d85643c25e74c6e4e9cc9a6cb5f3a1e
Vedansh Saini <vedanshsaini06@gmail.com>
2025-11-09 23:35:41 +0530

    resume builder project

---
+++ server/configs/db.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+    let mongodbURI = process.env.MONGODB_URI;
+
+    const projectName = "resume-builder";
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI environment variable not set");
+    }
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
+    }
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+  } catch (error) {
+    console.log("Error connecting on MongoDB:", error);
+  }
+};
+
+export default connectDB;
